4e68a563ef3774235c1079be5d88f7167cd22644
Titus Njuguna <titusnjuguna59@gmail.com>
2021-09-01 01:33:10 +0300

    First Commit

---
+++ JavaScript/assets/scripts/app.js
@@ -0,0 +1,11 @@
+/*declaring  and assigning variable*/
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult = currentResult + 105
+let calculatioDescription = `${defaultResult} + 105`;
+outputResult(currentResult, calculatioDescription);
+
+function addNum(num1, num2){
+    const result = num1 + num2;
+    return result
+}
---
+++ JavaScript/assets/scripts/assignment.js
@@ -0,0 +1,8 @@
+let userPut = 1995;
+let result 
+result = userPut + 18 ;
+result = result - 500;
+result = result * 20;
+result = result / 10;
+console.log(userPut);
+console.log(result);
