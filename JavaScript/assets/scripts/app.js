9330b1024285a09215c9b5292a0e359601d50834
Titus Njuguna <titusnjuguna59@gmail.com>
2021-09-01 13:39:25 +0300

    first commit

--- JavaScript/assets/scripts/app.js
+++ JavaScript/assets/scripts/app.js
@@ -1,11 +1,63 @@
 /*declaring  and assigning variable*/
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = currentResult + 105
-let calculatioDescription = `${defaultResult} + 105`;
-outputResult(currentResult, calculatioDescription);
+let logEntries = [];
+let logEntry = {} ;
 
-function addNum(num1, num2){
-    const result = num1 + num2;
-    return result
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+
 }
+
+function writeLog(oper,fNum,secdNum,resltNum){
+    logEntry = {
+        operation:oper,
+        firstNum : fNum,
+        secNum: secdNum,
+        resultant: resltNum
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+
+}
+
+
+
+function addNum(){
+    const gotNum = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${gotNum}`;
+    const initialNum = currentResult;
+    currentResult += gotNum;
+    outputResult(currentResult, calcDescription);
+    writeLog('ADD',gotNum,initialNum,currentResult)
+}
+
+function multiplyNum(){
+    const gotNum = getUserNumberInput();
+    const calcDescription = `${currentResult} * ${gotNum}`;
+    const initialNum = currentResult;
+    currentResult *= gotNum;
+    outputResult(currentResult, calcDescription);
+    writeLog('MULTIPLY',gotNum,initialNum,currentResult)
+}
+
+function divideNum(){
+    const gotNum = getUserNumberInput();
+    const calcDescription = `${currentResult} / ${gotNum}`;
+    const initialNum = currentResult;
+    currentResult /= gotNum;
+    outputResult(currentResult, calcDescription);
+    writeLog('DIVIDE',gotNum,initialNum,currentResult)
+}
+function subtractNum(){
+    const gotNum = getUserNumberInput();
+    const calcDescription = `${currentResult} - ${gotNum}`;
+    const initialNum = currentResult;
+    currentResult -= gotNum;
+    outputResult(currentResult, calcDescription);
+    writeLog('SUBTRACT',gotNum,initialNum,currentResult)
+}
+addBtn.addEventListener('click',addNum);
+subtractBtn.addEventListener('click',subtractNum);
+divideBtn .addEventListener('click',divideNum);
+multiplyBtn.addEventListener('click',multiplyNum);
--- JavaScript/assets/scripts/assignment.js
+++ JavaScript/assets/scripts/assignment.js
@@ -1,8 +1,25 @@
-let userPut = 1995;
+/*let userPut = 1995;
 let result 
 result = userPut + 18 ;
 result = result - 500;
 result = result * 20;
 result = result / 10;
 console.log(userPut);
-console.log(result);
+console.log(result);*/
+/*const task3Element = document.getElementById('task-3');*/
+
+/*Assignment2 functions
+function sayName(){
+    console.log('Hey there am learning javascript')
+}
+sayName();
+function takeName(name){
+    console.log(`Welcome home ${name}`)
+}
+takeName('tito');
+callNamaste.addEventListener('click',sayName);
+*/
+function trioMio(name,age,state){
+    console.log(`my name is ${name},am ${age} years old and I live in ${state}`)
+}
+trioMio('King Kabaka','27','QAlabama');
